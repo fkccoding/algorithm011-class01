d6a1a78d3aea7bd5cca2cbf891a978bdef520d38
fangcheng <fangcheng@xi-ai.com>
2020-06-24 09:38:37 +0800

    移动0的第二种方式

--- Week_01/MoveZero.java
+++ Week_01/MoveZero.java
@@ -16,4 +16,16 @@ public class MoveZero {
             nums[i] = 0;
         }
     }
+
+    public void moveZeroes2(int[] nums) {
+        int l = nums.length;
+        int j = 0;
+        for (int i = 0; i < l; ++i) {
+            if (nums[i] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = tmp;
+            }
+        }
+    }
 }
